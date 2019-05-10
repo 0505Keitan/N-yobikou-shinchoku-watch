64084e68f3f51e4b0b9bd1989af96e27751f31a5
0505Keitan <0505Keitan@gmail.com>
2019-05-10 13:05:51 +0900

    remove : 表示機能を作成したためLINE送信機能を削除

--- main.js
+++ main.js
@@ -16,15 +16,7 @@ for(i=0;i<sec.length;i++){
     var shinchoku = sec[i].innerText.split('\n');
     result.push(sub[0] + 'は' + shinchoku[1]);
 }
-var data = result.join("\n");
 var hyoujiData = result.join("<br>");
 
 var hyouji = document.getElementsByClassName('l-column-sub');
 hyouji[0].innerHTML="<div class='u-card u-recommendation-list'><div class='u-list-header typo-list-title'>" + userName[0].innerText + "さんの進捗状況</div><div class='u-card-inner'>"+ hyoujiData +"</div></div>"+hyouji[0].innerHTML;
-
-data = userName[0].innerText + "さんの進捗状況\n" + mon + "月" +  day + "日" + hour + "時" + min + "分" + seco + "秒現在\n\n" + data;
-var contentData = encodeURIComponent('content') + "=" + encodeURIComponent(data);
-url = "https://*************.com/LINE_send.php";
-request.open("POST", url);
-request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-request.send(contentData);
