8001c705d844f15770d08438db22ee45c5598444
0505Keitan <0505Keitan@gmail.com>
2019-05-10 15:29:16 +0900

    update : 表示を見やすく変更&fix : 読み込み時うまく表示されないバグを修正

--- main.js
+++ main.js
@@ -14,9 +14,13 @@ var userName = document.querySelectorAll(".name");
 for(i=0;i<sec.length;i++){
     var sub = sec[i].innerText.split(' ');
     var shinchoku = sec[i].innerText.split('\n');
-    result.push(sub[0] + 'は' + shinchoku[1]);
+    var res = "<div class='text'>"  + sub[0] + 'は' + shinchoku[1] + "</div>";
+    result.push(res);
 }
 var hyoujiData = result.join("<br>");
 
 var hyouji = document.getElementsByClassName('l-column-sub');
+if(hyouji === undefined){
+	location.reload();
+}
 hyouji[0].innerHTML="<div class='u-card u-recommendation-list'><div class='u-list-header typo-list-title'>" + userName[0].innerText + "さんの進捗状況</div><div class='u-card-inner'>"+ hyoujiData +"</div></div>"+hyouji[0].innerHTML;
