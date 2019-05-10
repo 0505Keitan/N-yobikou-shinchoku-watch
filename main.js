0224fe423454b28e2979da2d5a0eebb447c354bd
0505Keitan <0505Keitan@gmail.com>
2019-05-10 09:27:28 +0900

    ユーザー名と時間の表示に対応

--- main.js
+++ main.js
@@ -2,15 +2,22 @@ const request = new XMLHttpRequest();
 const result = [];
 var form = "";
 var content;
+var now = new Date();
+var mon = now.getMonth()+1;
+var day = now.getDate();
+var hour = now.getHours();
+var min = now.getMinutes();
+var seco = now.getSeconds();
 
 var sec = document.querySelectorAll(".show-my-course-item");
+var userName = document.querySelectorAll(".name");
 for(i=0;i<sec.length;i++){
     var sub = sec[i].innerText.split(' ');
     var shinchoku = sec[i].innerText.split('\n');
     result.push(sub[0] + 'は' + shinchoku[1]);
 }
 var data = result.join("\n");
-console.log(data);
+data = userName[0].innerText + "さんの進捗状況\n" + mon + "月" +  day + "日" + hour + "時" + min + "分" + seco + "秒現在\n\n" + data;
 var contentData = encodeURIComponent('content') + "=" + encodeURIComponent(data);
 url = "https://*************.com/LINE_send.php";
 request.open("POST", url);
